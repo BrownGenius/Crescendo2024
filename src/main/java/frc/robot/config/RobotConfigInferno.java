394c12562f0a51c30201cd5cb4b0c507602189a3
Ethan Porter <emanporter2009@gmail.com>
2024-09-30 20:30:19 -0400

    updated camera info

--- src/main/java/frc/robot/config/RobotConfigInferno.java
+++ src/main/java/frc/robot/config/RobotConfigInferno.java
@@ -137,8 +137,8 @@ public class RobotConfigInferno extends RobotConfig {
             "shooter",
             "1188",
             new Transform3d(
-                new Translation3d(-Units.inchesToMeters(9.5), 0, Units.inchesToMeters(14)),
-                new Rotation3d(0, Units.degreesToRadians(-32), Units.degreesToRadians(180)))));
+                new Translation3d(-Units.inchesToMeters(9.5), 0, Units.inchesToMeters(13.5)),
+                new Rotation3d(0, Units.degreesToRadians(-36.5), Units.degreesToRadians(180)))));
 
     cameras.add(
         new VisionCamera(
